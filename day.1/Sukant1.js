d6f0818f020f2a227a21f3e3c5fc76a17bebe3f6
Sukant22 <sukantkumar5942@gmail.com>
2025-08-25 00:26:52 +0530

    Create Sukant1.js

---
+++ day.1/Sukant1.js
@@ -0,0 +1,13 @@
+const classB = {
+   name: "Sukant Kumar",
+   branch: "Information Technology",
+   roll: "it18",
+   year: 3
+};
+
+console.log("Student Details (Class B)");
+console.log(⁠ Name  : ${classB.name} ⁠);
+console.log(⁠ Branch: ${classB.branch} ⁠);
+console.log(⁠ Roll  : ${classB.roll} ⁠);
+console.log(⁠ Year  : ${classB.year} ⁠);
+console.log("End");
