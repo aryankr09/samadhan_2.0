7663446bef6285594808d4ce57c060bcf0e99447
ARYAN KUMAR <aryankr7136@gmail.com>
2025-08-24 23:35:57 +0530

    Create aryan1.js
    Print student details from an object

---
+++ day.1/aryan1.js
@@ -0,0 +1,13 @@
+const classA = {
+   name : "Aryan",
+   branch : "Computer Science Engineering",
+   roll : "cs25",
+   year: "2nd" 
+
+};
+console.log("students Details");
+console.log(`Name : ${classA.name}` );
+console.log(`Branch : ${classA.branch}`);
+console.log(`Roll no : ${classA.roll}`);
+console.log(`Year : ${classA.year}`);
+console.log("End");
