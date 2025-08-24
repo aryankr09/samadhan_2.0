2ebb0545c855e85ee015ed356ec1fd4c5e869f9c
bhanuuu914 <bprataplodhi@gmail.com>
2025-08-24 11:27:20 -0700

    Create bhanu1.js

---
+++ day.1/bhanu1.js
@@ -0,0 +1,13 @@
+const classC = {
+   name: "Bhanu Lodhi",
+   branch: "Engineering",
+   roll: "me12",
+   year: 1
+};
+
+console.log("Student Details (Class C)");
+console.log(`Name  : ${classC.name}`);
+console.log(`Branch: ${classC.branch}`);
+console.log(`Roll  : ${classC.roll}`);
+console.log(`Year  : ${classC.year}`);
+console.log("End");
